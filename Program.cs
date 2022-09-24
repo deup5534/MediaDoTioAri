29b9e20b663394a6828e9b8ac8216cab80971525
etec adolpho berezin <e107ti@cps.sp.gov.br>
2022-09-23 21:50:44 -0300

    difícil demais

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿double resultado1, resultado2, resultado3, media;
+
+Console.WriteLine("Media Aritmetica de 3\n");
+
+Console.Write("digite seu primeiro numero:");
+resultado1 = Convert.ToDouble(Console.ReadLine());     
+
+
+Console.Write("digite seu segundo numero:");
+resultado2 = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("digite seu terceiro numero:");
+resultado3 = Convert.ToDouble(Console.ReadLine());
+
+media = (resultado1 + resultado2 + resultado3) /3;
+
+Console.WriteLine($"\nmedia: {media:N1}");
